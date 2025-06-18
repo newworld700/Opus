61e245d3e6fc2cb0d18a8536205c926b39704758
Abhishek Kumar <meaviishek@gmail.com>
2025-06-18 22:43:13 +0530

    ch

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,16 +1,20 @@
-"use client"
+"use client";
+
 import { checkAuth } from "@/store/slices/authSlice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 export default function AdminLayout({ children }) {
-    const dispatch=useDispatch()
-    useEffect(()=>{
-        dispatch(checkAuth())
-    },[dispatch])
-    return (
-      <html lang="en">
-        <body className="bg-gray-50">{children}</body>
-      </html>
-    );
-  }
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+  return (
+    <html lang="en">
+      <body className="bg-gray-50">{children}</body>
+    </html>
+  );
+}
